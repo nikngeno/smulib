0f8e933fcb9c674498181930bd12e7996715b597
nikngeno <nikngeno@gmail.com>
2025-02-10 22:24:59 -0800

    created the home page template

---
+++ MainP/script.js
@@ -0,0 +1,32 @@
+// Simple automatic slideshow functionality
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
+
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Move to next slide index
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  // Remove 'active' class from all dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // Display the current slide and highlight the corresponding dot
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  // Change slide every 4 seconds
+  setTimeout(showSlides, 4000);
+}
