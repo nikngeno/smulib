87d9628619a7388bd81d1226db09f16a5237f29b
nikngeno <nikngeno@gmail.com>
2025-02-18 15:51:28 -0800

    update to the search functionality

--- MainP/borrowbook.php
+++ MainP/borrowbook.php
@@ -1,4 +1,7 @@
 <?php
+
+$search = $_POST["search"];
+
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -11,4 +14,18 @@ if (!$conn) {
     die('Could not connect to MySQL: ' . mysqli_connect_error());
 }
 
-echo 'Connected successfully';
+//echo 'Connected successfully';
+
+$sql = "select * from books where BookName like '%$search%'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row["BookName"] . "  " . $row["BookAuthor"] . "  " . $row["PublicationYear"] . " " . $row["Genre"] . " " . $row["Price"] . " " . $row["Description"] . "<br>";
+    }
+} else {
+    echo "No records";
+}
+
+$conn->close();
