64a0a5179f119e162eefe01b47b2037e77e985b0
sprgrl4523 <captainmattoni2015@gmail.com>
2025-04-10 16:08:24 -0700

    update calendar

--- MainP/eventcalendar.php
+++ MainP/eventcalendar.php
@@ -13,80 +13,8 @@
 </head>
 <body>
 
-  <!-- Header and Navigation -->
-  <header>
-    <div class="nav-container">
-      <h1><a href= "index.html" style="color: inherit; text-decoration: none;">SMU Library</a></h1>
-      <nav>
-        <ul>
-          <!-- Menu Item 1 -->
-          <li class="dropdown">
-            <a href="#">Books</a>
-            <ul class="dropdown-content">
-              <li><a href="borrowbook.html">Borrow a Book</a></li>
-              <li><a href="#">Return a Book</a></li>
-              <li><a href="#">Sell a Book</a></li>
-              <li><a href="#">Buy a Book</a></li>
-            </ul>
-          </li>
-
-          <!-- Menu Item 2 -->
-          <li class="dropdown">
-            <a href="#">Service</a>
-            <ul class="dropdown-content">
-              <li><a href="bookappointment.html">Book an Appointment</a></li>
-              <li><a href="reserveroom.html">Reserve  Room</a></li>
-              <li><a href="requestlibrarian.html">Request A Librarian </a></li>
-            </ul>
-          </li>
-
-          <!-- Menu Item 3 -->
-          <li class="dropdown">
-            <a href="#">News and Events</a>
-            <ul class="dropdown-content">
-              <li><a href="calendar.html">Latest News</a></li>
-              <li><a href="#">Promotions</a></li>
-              <li><a href="#">Support the Library</a></li>
-            </ul>
-          </li>
-
-          <!-- Menu Item 4 -->
-          <li class="dropdown">
-            <a href="#">For You</a>
-            <ul class="dropdown-content">
-              <li><a href="#">Student</a></li>
-              <li><a href="#">Military</a></li>
-              <li><a href="#">Public</a></li>
-            </ul>
-          </li>
-
-          <!-- Menu Item 5 -->
-          <li class="dropdown">
-            <a href="#">About Us</a>
-            <ul class="dropdown-content">
-              <li><a href="team.html">Our Staff</a></li>
-              <li><a href="contact.html">Contact Us</a></li>
-              <li><a href="location.html">Location</a></li>
-              <!--<li><a href="#">Sub Item 3</a></li> -->
-            </ul>
-          </li>
-            <!-- Menu Item 6 -->
-            <li class="dropdown">
-              <a href="">My Account</a>
-              <ul class="dropdown-content">
-                <li><a href="sign_in.html">Sign in</a></li>
-                <li><a href="maincart.html">My Cart</a></li>
-              </ul>
-            </li>
-        </ul>
-      </nav>
-    </div>
-  
-  </header>
-
-  <main>
+  <?php include 'PHP/header.php'; ?>
     <br>
-    <h1>Library Calendar</h1>
     <div class = "calendar-wrap"> 
       <div class="calendar-body">
         <div class="month">      
@@ -173,7 +101,7 @@
             <div><p>Description</p></div>
         </div>
   
-          <!-- <div class="event-item">
+          <div class="event-item">
             <div class="event-header">
               <span >Date-time</span>
               <span class="event-title">Event Title</span>
@@ -189,12 +117,12 @@
               <span>Location</span>
             </div>
             <div><p>Description</p></div>
-          </div> -->
+          </div>
   
         </div>
       </div>
     </div>
-</main>
+
   <!-- Footer -->
   <footer>
     <p>&copy; 2025 SMU Library</p>
